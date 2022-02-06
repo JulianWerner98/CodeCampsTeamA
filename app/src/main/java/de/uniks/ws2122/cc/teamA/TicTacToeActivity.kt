4a6af9860ce98a4f257b37626b53cf0bfdfbb154
AdrianVollhardt <57192429+AdrianVollhardt@users.noreply.github.com>
2022-02-06 16:20:51 +0100

    create TicTacToe activty

---
+++ app/src/main/java/de/uniks/ws2122/cc/teamA/TicTacToeActivity.kt
@@ -0,0 +1,11 @@
+package de.uniks.ws2122.cc.teamA
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TicTacToeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe)
+    }
+}
